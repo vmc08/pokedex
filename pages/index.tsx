2b848db4f4c9bf98e115bf19ec399858dde50c73
vmc08 <vmc08@yahoo.com>
2021-05-14 23:00:34 +0800

    refactor(app): remove unnecessary component wrapper; move package to dev dependency

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,13 @@
-import { Flex, Text, Button } from "@chakra-ui/react"
+import { Text, Button } from "@chakra-ui/react"
 
 import Banner from 'components/Banner'
 
 export default function IndexPage() {
   return (
     <Banner closable>
-      <Flex fontSize="large">
-        <Text color="white">
-          <b>Did you buy a license yet?</b> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy.
-        </Text>
-      </Flex>
+      <Text color="white" fontSize="large">
+        <b>Did you buy a license yet?</b> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy.
+      </Text>
       <Button
         flexShrink={0}
         color="blue.600"

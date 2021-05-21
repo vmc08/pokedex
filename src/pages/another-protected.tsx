bd771f3b245947b1fde0a4758f423fea5b27a4b1
vmc08 <vmc08@yahoo.com>
2021-05-22 02:56:55 +0800

    refactor(app): add test protected pag

---
+++ src/pages/another-protected.tsx
@@ -0,0 +1,9 @@
+import { Container, Text } from "@chakra-ui/react"
+
+const AnotherProtectedPage = () => (
+  <Container maxW="container.xl">
+    <Text>Another Protected Route</Text>
+  </Container>
+)
+
+export default AnotherProtectedPage

f97f9bf3610009829fa6b7c33393c6f711a05861
vmc08 <vmc08@yahoo.com>
2021-06-09 19:17:01 +0800

    refactor(auth-guard): improve auth-guard, better way of checking private pages

--- src/components/AuthGuard.tsx
+++ src/components/AuthGuard.tsx
@@ -1,43 +1,39 @@
-import { PropsWithChildren, useEffect } from "react"
-import { Spinner, Flex } from "@chakra-ui/react"
-import Router from "next/router"
-import { useSession } from "next-auth/client"
+import { PropsWithChildren, useEffect } from "react";
+import { Spinner, Flex } from "@chakra-ui/react";
+import Router from "next/router";
+import { useSession } from "next-auth/client";
 
-import { TPageConfig } from "src/types/page"
+import { TPageConfig } from "src/types/page";
 
 const PageSpinner = () => (
   <Flex h="100vh" alignItems="center" justifyContent="center">
-    <Spinner
-      speed="0.65s"
-      emptyColor="gray.200"
-      color="blue.500"
-      size="lg"
-    />
+    <Spinner speed="0.65s" emptyColor="gray.200" color="blue.500" size="lg" />
   </Flex>
-)
+);
 
-const AuthGuard: React.FC<PropsWithChildren<Omit<TPageConfig, "showBanner">>> = ({ publicPage, children }) => {
-  const [session, loading] = useSession()
+const AuthGuard: React.FC<PropsWithChildren<Omit<TPageConfig, "showBanner">>> =
+  ({ isPublicPage: publicPage, children }) => {
+    const [session, loading] = useSession();
 
-  const handleRedirects = () => {
-    if (!publicPage) {
-      if (!session) Router.replace("/login")
-    } else {
-      if (session) Router.replace("/")
+    useEffect(() => {
+      if (!publicPage && !session && !loading) {
+        void Router.replace({
+          pathname: "/login",
+          query: {
+            callbackUrl: Router.asPath,
+          },
+        });
+      }
+    }, [loading]);
+
+    if (
+      loading || // while retrieving session
+      (!publicPage && !session) // page requires auth and no session, wait useEffect to redirect
+    ) {
+      return <PageSpinner />;
     }
-  }
 
-  useEffect(() => {
-    if (!loading) handleRedirects()
-  }, [loading])
+    return <>{children}</>;
+  };
 
-  if (loading) return <PageSpinner />
-  if (!publicPage && !session) return <PageSpinner />
-  if (publicPage && session) return <PageSpinner />
-
-  return <>
-    {children}
-  </>
-}
-
-export default AuthGuard
+export default AuthGuard;

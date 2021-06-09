9028da9b91f604970cf2809b669f5710c61b8e36
vmc08 <vmc08@yahoo.com>
2021-06-09 19:19:14 +0800

    refactor(login-page): move page redirection here for authenticated users accessing login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,20 +1,45 @@
-import { FC } from "react"
-import { Container, Box } from "@chakra-ui/react"
+import { FC } from "react";
+import { NextPageContext } from "next";
+import { getSession } from "next-auth/client";
+import { Container, Box } from "@chakra-ui/react";
 
-import LoginForm from 'src/modules/login/forms/LoginForm'
-import { TPageConfig } from 'src/types/page'
+import LoginForm from "src/modules/login/forms/LoginForm";
+import { TPageConfig } from "src/types/page";
 
 const LoginPage: FC & TPageConfig = () => {
   return (
-    <Container maxW="container.xl" minH="100vh" d="flex" alignItems="center" justifyContent="center">
+    <Container
+      maxW="container.xl"
+      minH="100vh"
+      d="flex"
+      alignItems="center"
+      justifyContent="center"
+    >
       <Box maxW="96" w="full">
         <LoginForm />
       </Box>
     </Container>
-  )
+  );
+};
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: true,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 }
 
-LoginPage.showBanner = false
-LoginPage.publicPage = true
+LoginPage.showBanner = false;
+LoginPage.isPublicPage = true;
 
-export default LoginPage
+export default LoginPage;

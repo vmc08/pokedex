c9d9b268e362b18146a535291f00d812ba0ee675
vmc08 <vmc08@yahoo.com>
2021-06-09 22:05:44 +0800

    chore(app): fix prisma seeder

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,27 +1,26 @@
-import { PrismaClient } from "@prisma/client"
+import prisma from "../src/utils/prisma";
+import stat from "./seeders/stat";
+import ability from "./seeders/ability";
+import type from "./seeders/type";
+import pokemon from "./seeders/pokemon";
+import user from "./seeders/user";
 
-import stat from './seeders/stat'
-import ability from './seeders/ability'
-import type from './seeders/type'
-import pokemon from './seeders/pokemon'
-import user from './seeders/user'
-
-const prisma = new PrismaClient()
-
-async function main() {
-  await stat()
-  await ability()
-  await type()
-  await pokemon()
-  await user()
+async function seed() {
+  await stat();
+  await ability();
+  await type();
+  await pokemon();
+  await user();
 }
 
-main()
-  .catch(e => {
-    console.error(e)
-    process.exit(1)
+seed()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
   })
   .finally(async () => {
-    await prisma.$disconnect()
-    process.exit()
-  })
+    await prisma.$disconnect();
+    process.exit();
+  });
+
+export default seed;
